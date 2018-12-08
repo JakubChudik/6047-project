08dd681be6ff5757005bbf867c53744348ea3b4a
Jakub <jchudik@mit.edu>
2018-12-08 12:46:30 -0500

    started PCA

--- data_analysis.py
+++ data_analysis.py
@@ -12,6 +12,8 @@ import sklearn
 from math import sqrt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
 def data_preprocessing(filename):
     '''
@@ -41,8 +43,42 @@ def supervised_learning(data):
     print('actual',list(y_test))
     print(sqrt(sklearn.metrics.mean_squared_error(y_test,pred_test)))
 
-def unsupervised_learning(data):
-    pass
+def unsupervised_learning(data, feature):
+    """
+    performs PCA on data and then clusters them
+    """
+    df = data.drop(['case_uuid', feature],axis= 1)
+    n_components = 2
+    
+    
+    pca = PCA(n_components)
+    principal_components = pca.fit_transform(df)
+    principal_df = pd.DataFrame(data = principal_components
+                 , columns = ['principal component 1', 'principal component 2'])
+    
+    #cluster and joint PCA with labels 
+    clusters = KMeans(n_clusters=2).fit(principal_df)
+    labels = pd.DataFrame({'target':clusters.labels_})
+    finalDf = pd.concat([principal_df, labels[['target']]], axis = 1)
+    
+    #plot
+    fig = plt.figure(figsize = (8,8))
+    ax = fig.add_subplot(1,1,1) 
+    ax.set_xlabel('Principal Component 1', fontsize = 40)
+    ax.set_ylabel('Principal Component 2', fontsize = 40)
+    ax.set_title('2 component PCA', fontsize = 40)
+    
+    targets = [0, 1]
+    colors = ['r', 'g']
+    
+    for target, color in zip(targets,colors):
+        indicesToKeep = finalDf['target'] == target
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+                   , finalDf.loc[indicesToKeep, 'principal component 2']
+                   , c = color
+                   , s = 50)
+    ax.legend(targets)
+    ax.grid()
 
 def main():
     data = data_preprocessing('cleanDataBreast.json')
